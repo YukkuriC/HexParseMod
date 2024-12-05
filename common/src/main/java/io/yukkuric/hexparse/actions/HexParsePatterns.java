eabe0e79a0f6239a40f1d6a808fe997502be55fa
YukkuriC <799433638@qq.com>
2024-12-05 22:01:39 +0800

    1.19 register methods

--- common/src/main/java/io/yukkuric/hexparse/actions/HexParsePatterns.java
+++ common/src/main/java/io/yukkuric/hexparse/actions/HexParsePatterns.java
@@ -1,34 +1,37 @@
 package io.yukkuric.hexparse.actions;
 
-import at.petrak.hexcasting.api.casting.ActionRegistryEntry;
-import at.petrak.hexcasting.api.casting.castables.Action;
-import at.petrak.hexcasting.api.casting.math.HexDir;
-import at.petrak.hexcasting.api.casting.math.HexPattern;
-import at.petrak.hexcasting.common.lib.hex.HexActions;
+import at.petrak.hexcasting.api.PatternRegistry;
+import at.petrak.hexcasting.api.spell.Action;
+import at.petrak.hexcasting.api.spell.math.HexDir;
+import at.petrak.hexcasting.api.spell.math.HexPattern;
 import io.yukkuric.hexparse.HexParse;
-import net.minecraft.core.Registry;
+import it.unimi.dsi.fastutil.Pair;
 import net.minecraft.resources.ResourceLocation;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class HexParsePatterns {
-    static Map<ResourceLocation, ActionRegistryEntry> CACHED = new HashMap<>();
+    static Map<ResourceLocation, Pair<HexPattern, Action>> CACHED = new HashMap<>();
 
-    public static final ActionRegistryEntry
+    public static final Pair<HexPattern, Action>
 //            CODE2FOCUS = wrap("code2focus", HexPattern.fromAngles("aqqqqqeawqwqwqwqwqwweeeeed", HexDir.EAST), ActionCode2Focus.INSTANCE),
             FOCUS2CODE = wrap("focus2code", HexPattern.fromAngles("aqqqqqwwewewewewewdqeeeeed", HexDir.EAST), ActionFocus2Code.INSTANCE);
 
     public static void registerActions() {
-        var reg = HexActions.REGISTRY;
-        for (var pair : CACHED.entrySet()) {
-            Registry.register(reg, pair.getKey(), pair.getValue());
+        try {
+            for (var pair : CACHED.entrySet()) {
+                var entry = pair.getValue();
+                PatternRegistry.mapPattern(entry.key(), pair.getKey(), entry.value());
+            }
+        } catch (PatternRegistry.RegisterPatternException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    static ActionRegistryEntry wrap(String name, HexPattern pattern, Action action) {
+    static Pair<HexPattern, Action> wrap(String name, HexPattern pattern, Action action) {
         var key = new ResourceLocation(HexParse.MOD_ID, name);
-        var val = new ActionRegistryEntry(pattern, action);
+        var val = Pair.of(pattern, action);
         CACHED.put(key, val);
         return val;
     }
