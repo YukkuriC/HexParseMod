e60849b9ad88e5a6639ed507860644523f8095be
YukkuriC <799433638@qq.com>
2024-12-08 18:44:56 +0800

    do backport

--- common/src/main/java/io/yukkuric/hexparse/actions/HexParsePatterns.java
+++ common/src/main/java/io/yukkuric/hexparse/actions/HexParsePatterns.java
@@ -6,6 +6,7 @@ import at.petrak.hexcasting.api.spell.math.HexDir;
 import at.petrak.hexcasting.api.spell.math.HexPattern;
 import io.yukkuric.hexparse.HexParse;
 import it.unimi.dsi.fastutil.Pair;
+import it.unimi.dsi.fastutil.objects.ObjectObjectImmutablePair;
 import net.minecraft.resources.ResourceLocation;
 
 import java.util.HashMap;
@@ -14,7 +15,7 @@ import java.util.Map;
 public class HexParsePatterns {
     static Map<ResourceLocation, Pair<HexPattern, Action>> CACHED = new HashMap<>();
 
-    public static final Pair<HexPattern, Action>
+    public static final ActionRegistryEntry
 //            CODE2FOCUS = wrap("code2focus", HexPattern.fromAngles("aqqqqqeawqwqwqwqwqwweeeeed", HexDir.EAST), ActionCode2Focus.INSTANCE),
             FOCUS2CODE = wrap("focus2code", HexPattern.fromAngles("aqqqqqwwewewewewewdqeeeeed", HexDir.EAST), ActionFocus2Code.INSTANCE);
     public static final ActionRegistryEntry REMOVE_COMMENTS = wrap("remove_comments", HexPattern.fromAngles("dadadedadadwqaeaqeww", HexDir.NORTH_EAST), ActionRemoveComments.INSTANCE);
@@ -30,10 +31,16 @@ public class HexParsePatterns {
         }
     }
 
-    static Pair<HexPattern, Action> wrap(String name, HexPattern pattern, Action action) {
+    static ActionRegistryEntry wrap(String name, HexPattern pattern, Action action) {
         var key = new ResourceLocation(HexParse.MOD_ID, name);
-        var val = Pair.of(pattern, action);
+        var val = new ActionRegistryEntry(pattern, action);
         CACHED.put(key, val);
         return val;
+    }
+
+    public static class ActionRegistryEntry extends ObjectObjectImmutablePair<HexPattern, Action> {
+        ActionRegistryEntry(HexPattern left, Action right) {
+            super(left, right);
+        }
     }
 }
