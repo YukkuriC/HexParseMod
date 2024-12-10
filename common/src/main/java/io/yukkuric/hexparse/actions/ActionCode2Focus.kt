870c333e31377308db45bae3625ae0b1a3b04225
YukkuriC <799433638@qq.com>
2024-12-11 01:29:10 +0800

    re-enable code2focus pattern, and limit in staff

--- common/src/main/java/io/yukkuric/hexparse/actions/ActionCode2Focus.kt
+++ common/src/main/java/io/yukkuric/hexparse/actions/ActionCode2Focus.kt
@@ -3,6 +3,7 @@ package io.yukkuric.hexparse.actions
 import at.petrak.hexcasting.api.spell.ConstMediaAction
 import at.petrak.hexcasting.api.spell.casting.CastingContext
 import at.petrak.hexcasting.api.spell.iota.Iota
+import at.petrak.hexcasting.api.spell.mishaps.MishapDisallowedSpell
 import io.yukkuric.hexparse.network.MsgHandlers
 import io.yukkuric.hexparse.network.MsgPullClipboard
 
@@ -10,6 +11,7 @@ object ActionCode2Focus : ConstMediaAction {
     override val argc = 0
 
     override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
+        if (ctx.source != CastingContext.CastSource.STAFF) throw MishapDisallowedSpell()
         MsgHandlers.SERVER.sendPacketToPlayer(ctx.caster, MsgPullClipboard(null, false));
         return listOf()
     }
--- common/src/main/java/io/yukkuric/hexparse/actions/ActionFocus2Code.kt
+++ common/src/main/java/io/yukkuric/hexparse/actions/ActionFocus2Code.kt
@@ -3,12 +3,14 @@ package io.yukkuric.hexparse.actions
 import at.petrak.hexcasting.api.spell.ConstMediaAction
 import at.petrak.hexcasting.api.spell.casting.CastingContext
 import at.petrak.hexcasting.api.spell.iota.Iota
+import at.petrak.hexcasting.api.spell.mishaps.MishapDisallowedSpell
 import io.yukkuric.hexparse.misc.CodeHelpers
 
 object ActionFocus2Code : ConstMediaAction {
     override val argc = 0
 
     override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
+        if (ctx.source != CastingContext.CastSource.STAFF) throw MishapDisallowedSpell()
         var player = ctx.caster
         var code = CodeHelpers.readHand(player)
         CodeHelpers.displayCode(player, code)
