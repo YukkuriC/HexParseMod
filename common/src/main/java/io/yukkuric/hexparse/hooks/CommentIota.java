762518f511dc7bf826119b2153f8c6f90b0f4678
YukkuriC <799433638@qq.com>
2024-12-18 00:53:18 +0800

    1.20.1 EXCLUSIVE: skip execution for comment iota

--- common/src/main/java/io/yukkuric/hexparse/hooks/CommentIota.java
+++ common/src/main/java/io/yukkuric/hexparse/hooks/CommentIota.java
@@ -1,8 +1,16 @@
 package io.yukkuric.hexparse.hooks;
 
+import at.petrak.hexcasting.api.casting.eval.CastResult;
+import at.petrak.hexcasting.api.casting.eval.ResolvedPatternType;
+import at.petrak.hexcasting.api.casting.eval.vm.CastingVM;
+import at.petrak.hexcasting.api.casting.eval.vm.SpellContinuation;
 import at.petrak.hexcasting.api.casting.iota.PatternIota;
+import at.petrak.hexcasting.common.lib.hex.HexEvalSounds;
 import net.minecraft.nbt.StringTag;
 import net.minecraft.nbt.Tag;
+import net.minecraft.server.level.ServerLevel;
+
+import java.util.List;
 
 public class CommentIota extends PatternIota {
     String comment;
@@ -14,5 +22,9 @@ public class CommentIota extends PatternIota {
 
     public Tag serialize() {
         return StringTag.valueOf(comment);
+    }
+
+    public CastResult execute(CastingVM vm, ServerLevel world, SpellContinuation continuation) {
+        return new CastResult(this, continuation, vm.getImage(), List.of(), ResolvedPatternType.ESCAPED, HexEvalSounds.NOTHING);
     }
 }
