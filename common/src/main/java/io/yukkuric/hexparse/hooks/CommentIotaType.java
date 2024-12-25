9ecceecfeba5cde3826e6073ec04f6a782735419
YukkuriC <799433638@qq.com>
2024-12-25 20:58:06 +0800

    hold shift hides comments

--- common/src/main/java/io/yukkuric/hexparse/hooks/CommentIotaType.java
+++ common/src/main/java/io/yukkuric/hexparse/hooks/CommentIotaType.java
@@ -15,6 +15,7 @@ import at.petrak.hexcasting.common.lib.hex.HexIotaTypes;
 import io.yukkuric.hexparse.HexParse;
 import io.yukkuric.hexparse.parsers.IotaFactory;
 import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.Registry;
 import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
@@ -71,8 +72,10 @@ public class CommentIotaType extends IotaType<CommentIota> {
     @Override
     public Component display(Tag tag) {
         var raw = tag.getAsString();
-        if (!IotaFactory.isGreatPatternPlaceholder(raw))
+        if (!IotaFactory.isGreatPatternPlaceholder(raw)) {
+            if (Screen.hasShiftDown()) return Component.empty();
             return Component.literal(raw).withStyle(ChatFormatting.DARK_GREEN);
+        }
         var len = raw.length();
         var loopSize = (int) Math.floor(len * Math.PI * 2);
         var ticker = (System.currentTimeMillis() / 20);
