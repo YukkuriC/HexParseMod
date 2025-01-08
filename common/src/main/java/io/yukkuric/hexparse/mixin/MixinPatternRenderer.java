98ebf2b404f41e4ffe874412b0fce79f974c778a
YukkuriC <799433638@qq.com>
2025-01-09 00:17:07 +0800

    mixin show opid

---
+++ common/src/main/java/io/yukkuric/hexparse/mixin/MixinPatternRenderer.java
@@ -0,0 +1,41 @@
+package io.yukkuric.hexparse.mixin;
+
+import at.petrak.hexcasting.interop.patchouli.AbstractPatternComponent;
+import at.petrak.hexcasting.interop.patchouli.LookupPatternComponent;
+import io.yukkuric.hexparse.mixin_interface.IGetOpName;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import vazkii.patchouli.api.IComponentRenderContext;
+import vazkii.patchouli.api.ICustomComponent;
+
+@Mixin(AbstractPatternComponent.class)
+public abstract class MixinPatternRenderer implements ICustomComponent {
+    @Inject(method = "render", at = @At("RETURN"))
+    public void render(GuiGraphics graphics, IComponentRenderContext context, float pticks, int mouseX, int mouseY, CallbackInfo ci) {
+        if (!Screen.hasShiftDown()) return;
+        if (!(this instanceof IGetOpName getter)) return;
+        var opName = getter.getOpName();
+        Component toRender = Component.literal(opName.toString())
+                .setStyle(context.getFont().withColor(ChatFormatting.BLACK));
+        graphics.drawString(Minecraft.getInstance().font, toRender, 5, 10, -1, false);
+    }
+
+    @Mixin(LookupPatternComponent.class)
+    public static abstract class AccessorLookup implements ICustomComponent, IGetOpName {
+        @Shadow
+        protected ResourceLocation opName;
+
+        public ResourceLocation getOpName() {
+            return opName;
+        }
+    }
+}
---
+++ common/src/main/java/io/yukkuric/hexparse/mixin_interface/IGetOpName.java
@@ -0,0 +1,7 @@
+package io.yukkuric.hexparse.mixin_interface;
+
+import net.minecraft.resources.ResourceLocation;
+
+public interface IGetOpName {
+    ResourceLocation getOpName();
+}
