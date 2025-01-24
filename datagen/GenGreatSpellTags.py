463522ed4e0a4018750e1bf8f3d11047f55b74b7
YukkuriC <799433638@qq.com>
2025-01-24 22:21:42 +0800

    1.20 way to mark great

---
+++ datagen/GenGreatSpellTags.py
@@ -0,0 +1,19 @@
+import os, json
+
+os.chdir(os.path.dirname(__file__))
+
+spells = ['compile']
+
+if 'prepare':
+    spells = ['hexparse:' + x for x in spells]
+    tags = ['can_start_enlighten', 'per_world_pattern', 'requires_enlightenment']
+    tags = [f'action/{x}.json' for x in tags]
+    tags = [*tags, *('hexcasting/' + x for x in tags)]
+    tags = ['../common/src/main/resources/data/hexcasting/tags/' + x for x in tags]
+
+if 'gen':
+    body = json.dumps({"values": spells}, separators=',:')
+    for t in tags:
+        os.makedirs(os.path.dirname(t), exist_ok=1)
+        with open(t, 'w', encoding='utf-8') as f:
+            f.write(body)
