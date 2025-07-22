663bb06afd5c7fa07a74e77efc3b1f0cacaffb42
YukkuriC <799433638@qq.com>
2025-07-22 21:26:36 +0800

    enum-ize commenting config

--- forge/src/main/java/io/yukkuric/hexparse/forge/config/HexParseConfigForge.java
+++ forge/src/main/java/io/yukkuric/hexparse/forge/config/HexParseConfigForge.java
@@ -14,8 +14,8 @@ public class HexParseConfigForge implements API {
     }
 
     @Override
-    public boolean parseCommentsAndIndents() {
-        return CfgParseIndentsEnabled.get();
+    public CommentParsingMode getCommentParsingMode() {
+        return CfgCommentParsingMode.get();
     }
 
     @Override
@@ -31,10 +31,12 @@ public class HexParseConfigForge implements API {
     public final ForgeConfigSpec.BooleanValue CfgParseIndentsEnabled;
     public final ForgeConfigSpec.BooleanValue CfgShowColorfulNested;
     public final ForgeConfigSpec.EnumValue<ParseGreatPatternMode> CfgParseGreatSpells;
+    public final ForgeConfigSpec.EnumValue<CommentParsingMode> CfgCommentParsingMode;
     public final ForgeConfigSpec.IntValue CfgParserBaseCost;
 
     public HexParseConfigForge(ForgeConfigSpec.Builder builder) {
         CfgParseGreatSpells = builder.comment(DESCRIP_PARSE_GREAT).defineEnum("ParseGreatSpells", ParseGreatPatternMode.BY_SCROLL);
+        CfgCommentParsingMode = builder.comment(DESCRIP_ENABLE_COMMENTS).defineEnum("CommentParsingMode", CommentParsingMode.MANUAL);
         CfgParseIndentsEnabled = builder.comment(DESCRIP_ENABLE_COMMENTS).define("ParseCommentsIndents", true);
         CfgParserBaseCost = builder.comment(DESCRIP_PARSER_BASE_COST).defineInRange("ParserBaseCost", 0, 0, 100000);
         CfgShowColorfulNested = builder.comment(DESCRIP_COLORFUL_NESTED).define("ShowColorfulNested", true);
