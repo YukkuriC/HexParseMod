9d71b3c7b7bafb1610d81fdadd3560665c77dd18
YukkuriC <799433638@qq.com>
2025-08-27 21:16:11 +0800

    refactor & fix dumb bug

--- common/src/main/java/io/yukkuric/hexparse/misc/CodeHelpers.java
+++ common/src/main/java/io/yukkuric/hexparse/misc/CodeHelpers.java
@@ -27,8 +27,8 @@ import java.util.function.Function;
 
 import static at.petrak.hexcasting.common.items.storage.ItemSpellbook.TAG_PAGES;
 
-public interface CodeHelpers {
-    class IOMethod {
+public class CodeHelpers {
+    public static class IOMethod {
         private final BiConsumer<ItemStack, CompoundTag> writer;
         private final Function<ItemStack, CompoundTag> reader;
         private ItemStack current;
@@ -78,19 +78,19 @@ public interface CodeHelpers {
         }
     }
 
-    static void doExtractMedia(ServerPlayer caster, long amount) {
+    public static void doExtractMedia(ServerPlayer caster, long amount) {
         var harness = IXplatAbstractions.INSTANCE.getStaffcastVM(caster, InteractionHand.MAIN_HAND);
         harness.getEnv().extractMedia(amount, false);
     }
 
-    static IOMethod getItemIO(ServerPlayer player) {
+    public static IOMethod getItemIO(ServerPlayer player) {
         if (player == null) return null;
         var ret = IOMethod.get(player.getMainHandItem());
         if (ret == null) ret = IOMethod.get(player.getOffhandItem());
         return ret;
     }
 
-    static void doParse(ServerPlayer player, String code, String rename) {
+    public static void doParse(ServerPlayer player, String code, String rename) {
         var target = getItemIO(player);
         if (target == null) return;
         var nbt = ParserMain.ParseCode(code, player);
@@ -98,7 +98,7 @@ public interface CodeHelpers {
         if (rename != null) target.rename(rename);
     }
 
-    static void doParse(ServerPlayer player, List<String> code, String rename) {
+    public static void doParse(ServerPlayer player, List<String> code, String rename) {
         var target = getItemIO(player);
         if (target == null) return;
         var nbt = ParserMain.ParseCode(code, player);
@@ -106,10 +106,10 @@ public interface CodeHelpers {
         if (rename != null) target.rename(rename);
     }
 
-    static String readHand(ServerPlayer player) {
+    public static String readHand(ServerPlayer player) {
         return readHand(player, StringProcessors.READ_DEFAULT);
     }
-    static String readHand(ServerPlayer player, StringProcessors.F post) {
+    public static String readHand(ServerPlayer player, StringProcessors.F post) {
         var target = getItemIO(player);
         if (target == null) return null;
         var iotaRoot = target.read();
@@ -118,39 +118,39 @@ public interface CodeHelpers {
         return ParserMain.ParseIotaNbt(iotaRoot, player, post);
     }
 
-    WeakReference<MinecraftServer> refreshedWorld = new WeakReference<>(null);
-    WeakReference<Boolean> refreshedLocal = new WeakReference<>(false);
+    static WeakReference<MinecraftServer> refreshedWorld = new WeakReference<>(null);
+    static boolean refreshedLocal = false;
 
-    static void autoRefresh(MinecraftServer server) {
-        if (server != refreshedWorld.get()) {
+    public static void autoRefresh(MinecraftServer server) {
+        if (!refreshedWorld.refersTo(server)) {
             var level = server.overworld();
-            HexParse.LOGGER.info("auto refresh for server: %s, level: %s".formatted(server.name(), level));
+            HexParse.LOGGER.info("auto refresh for server: {}, level: {}", server.name(), level);
             PatternMapper.init(level);
-            refreshedLocal.refersTo(true);
-            refreshedWorld.refersTo(server);
+            refreshedLocal = true;
+            refreshedWorld = new WeakReference<>(server);
         }
     }
 
-    static void autoRefreshLocal() {
-        if (refreshedLocal.get()) return;
+    public static void autoRefreshLocal() {
+        if (refreshedLocal) return;
         PatternMapper.initLocal();
-        refreshedLocal.refersTo(true);
+        refreshedLocal = true;
     }
 
-    static void displayCode(ServerPlayer player, String code) {
+    public static void displayCode(ServerPlayer player, String code) {
         if (player == null || code == null) return;
         var display = Component.translatable("hexparse.cmd.read.display", Component.literal(code).withStyle(ChatFormatting.WHITE)).withStyle(ChatFormatting.GREEN);
         player.sendSystemMessage(wrapClickCopy(display, code));
     }
 
-    static MutableComponent wrapClickCopy(MutableComponent component, String code) {
+    public static MutableComponent wrapClickCopy(MutableComponent component, String code) {
         return component.withStyle(
                 Style.EMPTY
                         .withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, code))
                         .withHoverEvent(HoverEvent.Action.SHOW_TEXT.deserializeFromLegacy(Component.translatable("chat.copy.click")))
         );
     }
-    static MutableComponent wrapClickSuggest(MutableComponent component, String command) {
+    public static MutableComponent wrapClickSuggest(MutableComponent component, String command) {
         return component.withStyle(
                 Style.EMPTY
                         .withClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, command))
@@ -158,7 +158,7 @@ public interface CodeHelpers {
         );
     }
 
-    static Component getPatternDisplay(ResourceLocation id, ServerLevel level) {
+    public static Component getPatternDisplay(ResourceLocation id, ServerLevel level) {
         var longName = id.toString();
 
         // check great pattern display
