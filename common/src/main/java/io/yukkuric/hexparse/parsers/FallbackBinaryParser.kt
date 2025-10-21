b617e9890cb0092bd874daf82fcdcd5a1799f85d
YukkuriC <799433638@qq.com>
2025-10-22 00:19:00 +0800

    compress/decompress bytes

--- common/src/main/java/io/yukkuric/hexparse/parsers/FallbackBinaryParser.kt
+++ common/src/main/java/io/yukkuric/hexparse/parsers/FallbackBinaryParser.kt
@@ -5,7 +5,11 @@ import io.yukkuric.hexparse.parsers.nbt2str.INbt2Str
 import io.yukkuric.hexparse.parsers.str2nbt.BaseConstParser.Prefix
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.network.FriendlyByteBuf
+import java.io.ByteArrayOutputStream
 import java.util.*
+import java.util.zip.DeflaterOutputStream
+import java.util.zip.InflaterOutputStream
+
 
 object FallbackBinaryParser {
     const val prefix = "nbt_"
@@ -15,7 +19,16 @@ object FallbackBinaryParser {
         override fun parse(node: String): CompoundTag {
             var raw = node.substring(4)
             for (p in REPLACERS) raw = raw.replace(p[1], p[0])
-            val bytes: ByteArray = Base64.getDecoder().decode(raw)
+            var bytes = Base64.getDecoder().decode(raw)
+
+            // decompress
+            val out = ByteArrayOutputStream()
+            val infl = InflaterOutputStream(out)
+            infl.write(bytes)
+            infl.flush()
+            infl.close()
+            bytes = out.toByteArray()
+
             val buf = FriendlyByteBuf(Unpooled.wrappedBuffer(bytes))
             return buf.readNbt()!!
         }
@@ -27,8 +40,17 @@ object FallbackBinaryParser {
         override fun parse(node: CompoundTag): String {
             val buf = FriendlyByteBuf(Unpooled.buffer())
             buf.writeNbt(node)
-            val bytes = ByteArray(buf.readableBytes())
+            var bytes = ByteArray(buf.readableBytes())
             buf.readBytes(bytes)
+
+            // compress
+            val out = ByteArrayOutputStream()
+            val defl = DeflaterOutputStream(out)
+            defl.write(bytes)
+            defl.flush()
+            defl.close()
+            bytes = out.toByteArray()
+
             var raw = Base64.getEncoder().encodeToString(bytes)
             for (p in REPLACERS) raw = raw.replace(p[0], p[1])
             return prefix + raw
