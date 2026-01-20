9777ca3876446cdc1b0a7fcd945d492d719e32d9
YukkuriC <799433638@qq.com>
2026-01-20 22:24:06 +0800

    dynamic special handler registry & version++

--- common/src/main/java/io/yukkuric/hexparse/api/HexParseAPI.java
+++ common/src/main/java/io/yukkuric/hexparse/api/HexParseAPI.java
@@ -1,8 +1,13 @@
 package io.yukkuric.hexparse.api;
 
+import at.petrak.hexcasting.api.casting.castables.SpecialHandler;
 import io.yukkuric.hexparse.parsers.ParserMain;
 import io.yukkuric.hexparse.parsers.nbt2str.INbt2Str;
+import io.yukkuric.hexparse.parsers.nbt2str.PatternParser;
 import io.yukkuric.hexparse.parsers.str2nbt.IStr2Nbt;
+import net.minecraft.nbt.CompoundTag;
+
+import java.util.function.BiFunction;
 
 public interface HexParseAPI {
     static void AddForthParser(IStr2Nbt p) {
@@ -10,5 +15,8 @@ public interface HexParseAPI {
     }
     static void AddBackParser(INbt2Str p) {
         ParserMain.AddBackParser(p);
+    }
+    static <T extends SpecialHandler> void AddSpecialHandlerBackParser(Class<T> cls, BiFunction<T, CompoundTag, String> func) {
+        PatternParser.AddSpecialHandlerBackParser(cls, func);
     }
 }
--- common/src/main/java/io/yukkuric/hexparse/parsers/nbt2str/PatternParser.java
+++ common/src/main/java/io/yukkuric/hexparse/parsers/nbt2str/PatternParser.java
@@ -2,8 +2,8 @@ package io.yukkuric.hexparse.parsers.nbt2str;
 
 import at.petrak.hexcasting.api.HexAPI;
 import at.petrak.hexcasting.api.casting.PatternShapeMatch;
+import at.petrak.hexcasting.api.casting.castables.SpecialHandler;
 import at.petrak.hexcasting.api.casting.eval.env.StaffCastEnv;
-import at.petrak.hexcasting.api.casting.iota.Iota;
 import at.petrak.hexcasting.api.casting.math.HexPattern;
 import at.petrak.hexcasting.common.casting.PatternRegistryManifest;
 import at.petrak.hexcasting.common.casting.actions.math.SpecialHandlerNumberLiteral;
@@ -18,8 +18,8 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.function.BiFunction;
 
 public class PatternParser implements INbt2Str, IPlayerBinder {
     static Map<String, String> SPECIAL_PATTERNS = new HashMap<>() {
@@ -29,7 +29,24 @@ public class PatternParser implements INbt2Str, IPlayerBinder {
             put("qqqaw", "\\");
         }
     };
-    static List<Iota> FOO_LIST = List.of();
+    static Map<Class<?>, BiFunction<?, CompoundTag, String>> SPECIAL_HANDLER_MAP = new HashMap<>();
+
+    public static <T extends SpecialHandler> void AddSpecialHandlerBackParser(Class<T> cls, BiFunction<T, CompoundTag, String> func) {
+        SPECIAL_HANDLER_MAP.put(cls, func);
+    }
+
+    static {
+        AddSpecialHandlerBackParser(SpecialHandlerMask.class, (hm, node) -> {
+            var maskBuilder = new StringBuilder("mask_");
+            for (var m : hm.getMask()) {
+                maskBuilder.append(m ? '-' : 'v');
+            }
+            return maskBuilder.toString();
+        });
+        AddSpecialHandlerBackParser(SpecialHandlerNumberLiteral.class, (hn, node) -> {
+            return "num_" + INbt2Str.displayMinimalStatic(hn.getX());
+        });
+    }
 
     @Override
     public boolean match(CompoundTag node) {
@@ -51,15 +68,8 @@ public class PatternParser implements INbt2Str, IPlayerBinder {
             opId = pm.key.location();
         } else if (matcher instanceof PatternShapeMatch.Special sm) {
             var handler = sm.handler;
-            if (handler instanceof SpecialHandlerMask hm) { // mask
-                var maskBuilder = new StringBuilder("mask_");
-                for (var m : hm.getMask()) {
-                    maskBuilder.append(m ? '-' : 'v');
-                }
-                return maskBuilder.toString();
-            } else if (handler instanceof SpecialHandlerNumberLiteral hn) { // num
-                return "num_" + displayMinimal(hn.getX());
-            }
+            var func = (BiFunction<SpecialHandler, CompoundTag, String>) SPECIAL_HANDLER_MAP.get(handler.getClass());
+            if (func != null) return func.apply(handler, node);
         }
 
         // by key
